29ae99c4d88014fb675d877ea77ad8a42c2daf58
Max <mhandler1991@gmail.com>
2019-10-20 19:08:27 -0700

    +article schema for scraping

---
+++ models/Article.js
@@ -0,0 +1,38 @@
+// REQUIREMENTS
+
+var mongoose = require("mongoose");
+
+// Schema Constructor
+var ArticleSchema = new Schema({
+
+    // `title` is required and of type String
+    title: {
+        // field type of string
+      type: String,
+        // field is required
+      required: true,
+        // field is unique (this will ensure that we are not re-populating the same data/articles in to the db)
+      unique: true
+    },
+
+    // `link` is required and of type String
+    link: {
+        // field type of string
+      type: String,
+        // fied is required
+      required: true
+    },
+
+
+    // note: {
+    //   type: Schema.Types.ObjectId,
+    //   ref: "Note"
+    // }
+    
+  });
+  
+  // This creates our model from the above schema, using mongoose's model method
+  var Article = mongoose.model("Article", ArticleSchema);
+  
+  // Export the Article model
+  module.exports = Article;
