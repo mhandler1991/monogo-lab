2602e050032ad0ac04aa8a40e37188b84996fee1
Max <mhandler1991@gmail.com>
2019-10-24 17:51:20 -0700

    +Modal for Notes

--- public/app.js
+++ public/app.js
@@ -1,15 +1,22 @@
 // Grab the articles as a json
-$.getJSON("/Articles", function(data) {
+// $.getJSON("/Articles", function(data) {
 
-    db.Article.find({})
-    .then(function(dbArticle) {
-        // Grab the result of the data pull
-      res.json(dbArticle);
-    //   Send to main page??
+//     db.Article.find({})
+//     .then(function(dbArticle) {
+//         // Grab the result of the data pull
+//       res.json(dbArticle);
+//     //   Send to main page??
 
-    })
-    .catch(function(err) {
-      res.json(err);
-    });
+//     })
+//     .catch(function(err) {
+//       res.json(err);
+//     });
 
-  });
+//   });
+
+
+// On Button Click Create POP-OUT MODEL
+$("#modalBttn").on('click',function(){
+    console.log("Modal was Triggered")
+    $('#modal').modal('show');
+});
